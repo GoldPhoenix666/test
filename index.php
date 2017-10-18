aa603cc31896ae9f0c0a034c7d2265c3c04626d4
DESKTOP-1IGS2AN\Niall <niall@food-surveys.com>
2017-10-18 11:02:19 +0100

    no message

--- index.php
+++ index.php
@@ -8,18 +8,92 @@ include('C:\xampp\connection\connect.php');
 <!DOCTYPE html>
 <html>
 <head>
+
+
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
+
+
+	<script type="text/javascript" >
+		google.charts.load('current', {'packages':['corechart']});
+		google.charts.setOnLoadCallback(drawChart);
+
+		function drawChart() {
+
+		var piedata = new google.visualization.DataTable();
+
+		piedata.addColumn('string', 'time');
+		piedata.addColumn('number', 'hours');
+
+<?php echo $personactivitiesdata ?>
+
+		var options = {title: 'List of all Infomation', sliceVisibilityThreshold: 0};
+
+		var chart = new google.visualization.PieChart(document.getElementById('piedatadiv'));
+		chart.draw(piedata, options);};
+	</script>
+
+
+	<script type="text/javascript">
+  
+		google.charts.setOnLoadCallback(drawVisualization);
+
+		function drawVisualization() {
+
+     	var namechartdata = new google.visualization.DataTable();
+        namechartdata.addColumn('string', 'name');
+        namechartdata.addColumn('number', 'Quantity');
+    
+<?php echo $peoplechart ?>  
+
+      	var options = {title: 'Peoples activities', chartArea: {width: '60%'},
+      	hAxis: {
+		title: '# of Activities', minValue: 0},
+        vAxis: {title: 'Name', direction:'-1'},
+        annotation:{1:{style:'line'}}};
+      	var chart = new google.visualization.BarChart(document.getElementById('peoplediv'));
+      	chart.draw(namechartdata, options);
+		};
+	</script>
+
+
 </head>
 <body>
 
+<h1>Summary Inspection Report - Atherstone</h1>
+
+<div class="row-fluid">
+	<div class="span9" style="border: 1px solid red;">
+
+	</div>
 
 
+	<div class="span3" style="border: 1px solid blue">
+		<?php echo $peopletable ?>
+	</div>
+</div>
 
+<div class="row-fluid" >
+	<div class="span9" style="border: 1px solid green">
+		
+	</div>
+</div>
+<br />
+
+<div class="row-fluid">
+	<div class="span9" style="border: 1px solid red">
+		
+	</div>
+	
+
+	<div class="span3" style="border: 1px solid blue">
+		<?php echo $occurrencetable ?>
+	</div>
+</div>
 
 <div class="row-fluid">
 	<div class="span6" style="border: 0px red solid; text-align: center;">
